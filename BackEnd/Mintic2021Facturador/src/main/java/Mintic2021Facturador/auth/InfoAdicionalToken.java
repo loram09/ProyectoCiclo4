5707ca7b1b67c471be05ac6fecab5081c044f6e9
johaCatRG <94725439+johaCatRG@users.noreply.github.com>
2021-11-30 02:03:45 -0500

    Update InfoAdicionalToken.java

--- BackEnd/Mintic2021Facturador/src/main/java/Mintic2021Facturador/auth/InfoAdicionalToken.java
+++ BackEnd/Mintic2021Facturador/src/main/java/Mintic2021Facturador/auth/InfoAdicionalToken.java
@@ -25,7 +25,7 @@ public class InfoAdicionalToken  implements TokenEnhancer{
 		Usuario usuario=usuarioService.findByUsername(authentication.getName());
 		
 		Map<String,Object> info=new HashMap<>();
-		info.put("info_adicional","Hola que tal: !".concat(authentication.getName()));
+		info.put("info_adicional","Bienvenido: !".concat(authentication.getName()));
 		info.put("nombre",usuario.getNombre());
 		info.put("apellido",usuario.getApellido());
 		info.put("emai",usuario.getEmail());
